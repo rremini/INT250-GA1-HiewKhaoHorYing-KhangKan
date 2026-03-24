54c7237a9f2c1c9b63c27047f577800a3ee6fa03
Pakin Tangsatiankij <177916027+Payya007@users.noreply.github.com>
2026-03-24 10:25:50 +0700

    add error handling when blog is empty  array

--- src/views/blog/BlogList.vue
+++ src/views/blog/BlogList.vue
@@ -1,27 +1,22 @@
 <script setup>
 import BlogCard from "../../components/blog/BlogCard.vue";
 import { blogs } from "../../data/blog/blogData.js"
-
-
 </script>
 
+<template>
+    <div class="max-w-3xl mx-auto p-6">
+        <h1 class="text-2xl font-bold mb-6">
+            คลังความรู้
+        </h1>
 
-
-    <template>
-        
-        <div class="max-w-3xl mx-auto p-6">
-
-            <h1 class="text-2xl font-bold mb-6">
-                คลังความรู้
-            </h1>
-
-            <div class="space-y-4">
-
+        <div class="space-y-4">
+            <template v-if="blogs && blogs.length > 0">
                 <BlogCard v-for="blog in blogs" :key="blog.id" :blog="blog" />
+            </template>
 
+            <div v-else class="text-center py-10 border-2 border-dashed border-gray-200 rounded-lg">
+                <p class="text-gray-500">ไม่พบข้อมูลบทความในขณะนี้</p>
             </div>
-
         </div>
-
-    </template>
-
+    </div>
+</template>
